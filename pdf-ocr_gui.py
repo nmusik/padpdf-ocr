28bf93d82ffe0ab58b2e603d2533a53f72de58a8
nmusik <97416425+nmusik@users.noreply.github.com>
2022-01-12 11:10:07 +0800

    Create pdf-ocr_gui.py
    Basic GUI for pdf-ocr

---
+++ pdf-ocr_gui.py
@@ -0,0 +1,5 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from PyQt5.QtWidgets import QFileDialog, QApplication, QPushButton, QMessageBox,QMainWindow,QInputDialog,QTextEdit
+from PyQt5.Qt import QThread,QMutex,pyqtSignal
+import pdf-ocr
